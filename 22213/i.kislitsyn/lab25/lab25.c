aabe3c15ed2c6f81a323cc4899195b3d8995cefb
Ilya Kislitsyn <i.kislitsyn@g.nsu.ru>
2023-12-09 10:39:51 +0700

    init

---
+++ 22213/i.kislitsyn/lab25/lab25.c
@@ -0,0 +1,50 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    int descriptors[2];
+    if (pipe(descriptors) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+    pid_t pid = fork();
+    switch (pid) {
+        case -1:
+            perror("fork");
+            exit(EXIT_FAILURE);
+        case 0: //child process
+            close(descriptors[1]); // closing the write end.
+            int read_fd = descriptors[0];
+            char ch;
+            // char upper_message[MSG_SIZE];
+            while (read(read_fd, &ch, 1)) {
+                printf("%c", toupper(ch));
+            }
+            printf("\n");
+            close(read_fd);
+            break;
+        default:                   // parent process
+            close(descriptors[0]); // closing the read end.
+            int write_fd = descriptors[1];
+            char* message = "KJGGsgksjgssLGwsg F\0";
+            for (char* pointer = message; *pointer != '\0'; pointer++)
+            {
+                if(write(write_fd, pointer, 1) == 0) {
+                    perror("write");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            close(write_fd);
+
+            if (waitpid(pid, NULL, 0) == -1)
+            {
+                perror("wait");
+                exit(EXIT_FAILURE);
+            }
+    }
+    exit(EXIT_SUCCESS);
+}
