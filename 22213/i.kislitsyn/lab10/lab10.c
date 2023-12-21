64fa44d7924807e39f91a66771bb7744a4a7aa84
Kislitsyn Ilya <kislitsyn.ig@gmail.com>
2023-12-21 11:05:25 +0700

    lab10

---
+++ 22213/i.kislitsyn/lab10/lab10.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+    pid_t pid;
+    int status;
+    if (argc < 2)
+    {
+        printf("No command passed\n");
+        exit(EXIT_FAILURE);
+    }
+    switch (pid = fork())
+    {
+    case -1:
+        perror("fork error");
+        exit(EXIT_FAILURE);
+    case 0:
+        execvp(argv[1], argv + 1);
+        perror("Exec error");
+        exit(EXIT_FAILURE);
+
+    default:
+        if (waitpid(pid, &status, 0) == -1)
+        {
+            perror("waitpid error");
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            printf("\nCode %d\n", status);
+        }
+    }
+    exit(EXIT_SUCCESS);
+}
