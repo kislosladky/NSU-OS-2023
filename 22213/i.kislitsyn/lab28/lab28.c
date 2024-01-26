daac9f1898d52a9ab6fa2f3a4c595a096a5c300b
Kislitsyn Ilya <kislitsyn.ig@gmail.com>
2024-01-27 01:34:41 +0700

    lab28

--- 22213/i.kislitsyn/lab28/lab28.c
+++ 22213/i.kislitsyn/lab28/lab28.c
@@ -1,29 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <libgen.h>
 #include <unistd.h>
 
-int main() {
-    int nums[100];
+int main()
+{
     FILE *fptrs[2];
     srand((unsigned int)getpid());
-    for (int i = 0; i < 100; i++)
+
+    if (p2open("/bin/sort -n", fptrs) < 0)
     {
-        nums[i] = rand() % 100;
+        perror("p2open error");
+        exit(EXIT_FAILURE);
     }
 
-    p2open("/bin/sort", fptrs);
     char str[4];
     for (int i = 0; i < 100; i++)
     {
-        sprintf(str, "%d", nums[i]);
-        fputs(str, fptrs[0]);
+        fprintf(fptrs[0], "%d\n", rand() % 100);
     }
     fclose(fptrs[0]);
 
-    for (int i = 0; i < 100; i++)
+    int index = 1;
+    for (int i = 0; i < 10; i++)
     {
-        fgets(str, 3, fptrs[1]);
-        printf("%s ", str);
+        for (int j = 0; j < 10; j++)
+        {
+            if (fgets(str, 4, fptrs[1]) == NULL)
+            {
+                fprintf(stderr, "fgets error\n");
+                fclose(fptrs[1]);
+                exit(EXIT_FAILURE);
+            }
+
+            if (index < 2 && str[index] != '\n')
+            {
+                index++;
+            }
+            str[index] = '\0';
+            printf("%3s", str);
+        }
+        printf("\n");
     }
+    fclose(fptrs[1]);
+
+    exit(EXIT_SUCCESS);
 }
